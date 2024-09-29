ef1472075bd29f7b1e755d1ae71a4180ac214fd2
axelromfelt <114027887+axelromfelt@users.noreply.github.com>
2024-09-29 14:28:49 +0200

    chore: delete 123.html

---
+++ generate_list.py
@@ -0,0 +1,19 @@
+import json
+
+import os
+EXTENSIONS = ['.jpg', '.png', '.jpeg', '.gif']
+# get all files inside a specific folder
+
+data = []
+dir_path = r'./images'
+for path in os.listdir(dir_path):
+   # if path.is_file():
+    for extension in EXTENSIONS:
+        print(str(path), extension)
+        if str(path).endswith(extension):
+            data.append(path)  # .name)
+
+# Convert into JSON
+# File name is mydata.json
+with open("my_data.json", "w") as final:
+    json.dump(data, final)
